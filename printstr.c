0f562aaa8e14946fadf838869580c7c768a3b558
Sanni <sandae20v@gmail.com>
2023-11-14 21:36:15 +0100

    Initial code for 's' handler

---
+++ printstr.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * printstr - Prints a string
+ *
+ * @str: String to print
+ *
+ * Return: Length of character of
+ * str
+ *
+ */
+int printstr(va_list str)
+{
+	char *st;
+	int i, len;
+
+	st = va_arg(str, char *);
+
+	/* Handles NULL */
+	if (st == NULL)
+	{
+		st = "(null)";
+		len = _strlen(st);
+		for (i = 0; i < len; i++)
+		{
+			_putchar(st[i]);
+		}
+	}
+	else
+	{
+		len = _strlen(st);
+		for (i = 0; i < len; i++)
+			_putchar(st[i]);
+	}
+
+	return (len);
+}
